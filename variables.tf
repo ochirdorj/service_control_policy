1701ebcbf826068f862c7ec09d419b4adcc58a33
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-14 21:19:36 -0500

    var file copied

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "scp_name" {
+  type = string
+}
+
+variable "scp_description" {
+  type = string
+}
+
+variable "scp_path" {
+  type = string
+}
+
+variable "scp_type" {
+  type = string
+}
+
+variable "include_root" {
+  type = bool
+  description = "include root_id = true, exlclude root_id = false"
+}
+
+variable "target_ou_names" {
+  type = list(string)
+  description = "scp target ou name, leave it blank, it you don't want to attach scp to ou"
+}
+
+variable "target_account_names" {
+  type = list(string)
+  description = "scp target account names, leave it blank, it you don't want to attach scp to account"
+}
+
